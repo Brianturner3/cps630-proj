d1eebaba91de0f062e3b9e159a7bbcf1164010f8
Brian Turner <brianturner@Brians-MacBook-Pro.local>
2017-03-26 22:32:17 -0400

    login and signup

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 myApp = angular.module('myApp',['ngRoute']);
+var url = 'mongodb://turner:cupcake@ds129010.mlab.com:29010/cps630proj';
 myApp.config(['$routeProvider', '$locationProvider', function($routeProvider,$locationProvider){
 	$routeProvider.when("/login", {
 		templateUrl : "views/login.html",
@@ -14,16 +15,65 @@ myApp.config(['$routeProvider', '$locationProvider', function($routeProvider,$lo
 }]);
 
 myApp.controller('loginController',function($scope,$http){
+
+
 	$scope.loginUser = function(){
+
+		var user = {
+			email : $scope.loginEmail,
+			password : $scope.loginPassword,
+		}
+		var refresh = function(){
+			$scope.name = "";
+			$scope.email = "";
+			$scope.password = "";
+			confirm_password = "";
+		}
 		console.log("logging user in");
-		$http.post('/api/loginUser'.then(function(response){
+		$http.post('/api/loginUser', user).then(function(response){
 			console.log(response);
-		}));
+			refresh();
+		});
 	}
+
 });
 
-myApp.controller('signUpController', function($scope){
-	
+myApp.controller('signUpController', function($scope,$http){
+
+	$scope.registerUser = function(){
+
+		var validateEmail = function(email){
+   			var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    		return re.test(email);
+		}
+		var refresh = function(){
+			$scope.name = "";
+			$scope.email = "";
+			$scope.password = "";
+			confirm_password = "";
+		}	
+		var user = {
+			name : $scope.name,
+			email : $scope.email,
+			password : $scope.password,
+			confirm_password : $scope.confirmPassword
+		}
+		if(!validateEmail(user.email)){
+			alert("Email incorrect");
+		}
+		else if(user.password != user.confirm_password){
+			alert("passwords do not match!");
+		}else{
+			console.log(user);
+			$http.post('/api/registerUser', user).then(function(response){
+				console.log(response);
+				refresh();
+			});
+		}
+	}
+
+
+
 });
 
 
--- server.js
+++ server.js
@@ -3,11 +3,13 @@ var app = express();
 var mongojs = require('mongojs');
 var bodyparser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://turner:cupcake@ds129010.mlab.com:29010/cps630proj';
 var db = mongojs('mongodb://turner:cupcake@ds129010.mlab.com:29010/cps630proj',['users']);
 test = require('assert');
 // Connection url
 var url = 'mongodb://turner:cupcake@ds129010.mlab.com:29010/cps630proj';
 app.use(express.static(__dirname + '/public'));
+app.use(bodyparser.json());
 // Connect using MongoClient
 MongoClient.connect(url, function(err, db) {
   // Get an additional db
@@ -23,13 +25,55 @@ app.get('/api/users',function(req,res){
 	console.log("I recieved a GET request");
 
 	db.users.find(function(err, docs){
-		if(err){
+		if(err){  
 			throw err.message;
 		}else{
 			console.log(docs);
 			res.json(docs);
 		}
 	})
+});
+
+app.post('/api/registerUser', function(req,res){
+  console.log("Recieved Registration");
+  console.log(req.body.name);
+  var email = req.body.email;
+  var password = req.body.password;
+
+  db.users.find({'email' : email},function(err,docs){
+    if(err){
+      throw err.message;
+    }else if(docs.length == 1){
+      console.log("User already exists");
+    }else if(docs.length == 0){
+      console.log("User does not exist");
+      insertIntoDb(req.body)
+    }
+  });
+
+  var insertIntoDb = function(request){
+    db.users.insert(request, function(err,docs){
+      res.json(docs);
+      console.log("inserted into db");
+    })
+  }
+});
+
+app.post('/api/loginUser', function(req,res){
+  var email = req.body.email;
+  var password = req.body.password;
+
+  console.log(email + " " + password);
+
+  db.users.find({'email' : email, 'password' : password},function(err,docs){
+    if(err){
+      throw err.message;
+    }else if(docs.length == 1){
+      console.log("Logging in");
+    }else{
+      console.log("Can't find you");
+    }
+  })
 });
   //db.close();
   //console.log("connected");
