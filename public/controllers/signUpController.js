f682086f4a9cfa9cfda2126c2eecf9d642c00adf
Ivanna Rasay <rasay.ivanna@hotmail.com>
2017-03-29 21:20:27 -0400

    Add files via upload

---
+++ public/controllers/signUpController.js
@@ -0,0 +1,36 @@
+angular.module('myApp')
+	.controller('signUpController', function($scope,$http){
+
+	$scope.registerUser = function(){
+
+		var validateEmail = function(email){
+   			var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    		return re.test(email);
+		}
+		var refresh = function(){
+			$scope.name = "";
+			$scope.email = "";
+			$scope.password = "";
+			confirm_password = "";
+		}	
+		var user = {
+			name : $scope.name,
+			email : $scope.email,
+			password : $scope.password,
+			confirm_password : $scope.confirmPassword,
+			favoriteSchools : []
+		}
+		if(!validateEmail(user.email)){
+			alert("Email incorrect");
+		}
+		else if(user.password != user.confirm_password){
+			alert("passwords do not match!");
+		}else{
+			console.log(user);
+			$http.post('/api/registerUser', user).then(function(response){
+				console.log(response);
+				refresh();
+			});
+		}
+	}
+});
